8c47eda09c3bfe272144290495b6470385e6d200
Emily Quill <emilyquill@outlook.com>
2016-04-12 15:45:32 +1000

    Added Migration for Users

--- BurningAirlines-app/db/schema.rb
+++ BurningAirlines-app/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160412053706) do
+ActiveRecord::Schema.define(version: 20160412054001) do
 
   create_table "flights", force: :cascade do |t|
     t.string   "origin"
@@ -28,6 +28,12 @@ ActiveRecord::Schema.define(version: 20160412053706) do
     t.string   "name"
     t.integer  "columns"
     t.integer  "rows"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
