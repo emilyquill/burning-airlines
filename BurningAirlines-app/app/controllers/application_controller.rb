185a9c2d74cdaab2097f27b62b5a2ce4e0d7c448
Samantha Diamond <samanthadiamond89@gmail.com>
2016-04-13 15:33:42 +1000

    session authorisation

--- BurningAirlines-app/app/controllers/application_controller.rb
+++ BurningAirlines-app/app/controllers/application_controller.rb
@@ -1,4 +1,9 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+  before_action :fetch_user
+  # before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
+  before_action :correct_user,   only: [:edit, :update]
+  before_action :authorise, :except => [:new, :create, :home]
 
   def log_in user
     session[:user_id] = user.id
@@ -17,7 +22,22 @@ class ApplicationController < ActionController::Base
    redirect_to(session[:forwarding_url] || default)
    session.delete(:forwarding_url)
  end
+
+ def store_location
+     session[:forwarding_url] = request.url if request.get?
+  end
+
+  private
+  def fetch_user
+    @current_user = User.find_by :id => session[:user_id] if session[:user_id].present?
+    session[:user_id] = nil unless @current_user.present?
+  end
+
+  def authorise
+  redirect_to root_path unless (@current_user.present?)
+  end
+
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
+
 end
--- BurningAirlines-app/app/controllers/sessions_controller.rb
+++ BurningAirlines-app/app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       log_in user
-      redirect_back_or user
+      redirect_back_or search_path
     else
       flash.now[:danger] = 'Invalid username or password'
       render 'new'
@@ -15,7 +15,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-    log_out
+    log_out 
     redirect_to root_path
   end
 
